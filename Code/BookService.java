8a1b7831e4376d29f1522111868ab6320c581bbb
Revvrel <khairul.m@hotmail.com>
2023-12-05 13:30:36 +0800

    Added Book classes

---
+++ Code/BookService.java
@@ -0,0 +1,18 @@
+package sg.ntu.edu.simplecrm;
+
+import java.util.ArrayList;
+
+public interface BookService {
+
+   Book createBook(Book book);
+
+   Book getBook(int id);
+
+   ArrayList<Book> getAllBooks();
+
+   Book updateBook(int id, Book book);
+
+   void deleteBook(int id);
+
+   ArrayList<Book> searchBooks(String title);
+}
