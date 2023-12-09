6137e5fcd8af419b69223af67837792c955bb750
Wei Cong TONG <137802716+eggOnion@users.noreply.github.com>
2023-12-10 00:49:58 +0800

    Added login authentication & global exception handler for both book & learner class

---
+++ librarybookingsystem/src/main/java/com/example/librarybookingsystem/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,67 @@
+package com.example.librarybookingsystem.exceptions;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.example.librarybookingsystem.BookNotFoundException;
+import com.example.librarybookingsystem.entities.ErrorResponse;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // this is handler for LearnerNotFoundException
+    @ExceptionHandler({ LearnerNotFoundException.class, BookNotFoundException.class })
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(LearnerNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), LocalDateTime.now());    
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    // [Activity 1 - Refactor]
+    // @ExceptionHandler(InteractionNotFoundException.class)
+    // public ResponseEntity<ErrorResponse>
+    // handleInteractionNotFoundException(CustomerNotFoundException ex){
+    // ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(),
+    // LocalDateTime.now());
+    // return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    // }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<ErrorResponse> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex) {
+        ErrorResponse errorResponse = new ErrorResponse("Item does not exist.", LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        // Get a list of all validation errors from the exception object
+        List<ObjectError> validationErrors = ex.getBindingResult().getAllErrors();
+
+        // Create a StringBuilder to store all error messages
+        StringBuilder sb = new StringBuilder();
+
+        for (ObjectError error : validationErrors) {
+            sb.append(error.getDefaultMessage() + ".");
+        }
+
+        ErrorResponse errorResponse = new ErrorResponse(sb.toString(), LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        // We can log the exception here
+        // logger.error(ex.getMessage(), ex);
+
+        // return generic error message;
+        ErrorResponse errorResponse = new ErrorResponse("Something went wrong", LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
