c86e83720128892af2e353dc50118585bec6c6fe
Wei Cong TONG <137802716+eggOnion@users.noreply.github.com>
2023-12-09 10:21:49 +0800

    change user to learner

--- librarybookingsystem/src/main/java/com/example/librarybookingsystem/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.librarybookingsystem.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-  public UserNotFoundException(int id){
-    super("Could not find user with id: " + id + ".");
-  }
-    
-}
